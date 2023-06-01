71d5972cfd5484995df855ff8c529d1331188259
Ivan <vantei1999@mail.ru>
2023-06-01 12:51:21 +0300

    2 commit

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                double a;
+                double b;
+                double total;
+                char oper;
+
+                Console.WriteLine("Введите первое число:");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Введите оператор:");
+                oper = Convert.ToChar(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число:");
+                b = Convert.ToDouble(Console.ReadLine());
+
+                if (oper == '+')
+                {
+                    total = a + b;
+                    Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+                }
+
+                else if (oper == '-')
+                {
+                    total = a - b;
+                    Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
+                }
+
+                else if (oper == '*')
+                {
+                    total = a * b;
+                    Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
+                }
+
+                else if (oper == '/')
+                {
+                    
+                    
+                        total = a / b;
+                        Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+                   
+                   
+                }
+               
+            
+        }
+    }
+}
